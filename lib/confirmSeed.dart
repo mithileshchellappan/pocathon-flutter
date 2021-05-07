f23ed5a44d5ff5ee8c43f9c71aedfcc01c5177eb
Raajeev Chandran <raajeevchandran787@gmail.com>
2021-05-07 22:32:04 +0530

    validating seed phrase

--- lib/confirmSeed.dart
+++ lib/confirmSeed.dart
@@ -8,8 +8,18 @@ class ConfirmSeed extends StatefulWidget {
   @override
   _ConfirmSeedState createState() => _ConfirmSeedState();
 }
-List<String> checkStr = [''];
+
 class _ConfirmSeedState extends State<ConfirmSeed> {
+  List<String> checkStr = [];
+  List<Map<String, dynamic>> chipProps = [];
+  List<Widget> tappedChips = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    chipProps = List.generate(widget.phrases.length,
+        (index) => {"label": widget.phrases[index], "isSelected": false});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,41 +42,97 @@ class _ConfirmSeedState extends State<ConfirmSeed> {
             ),
           ),
           Expanded(
-            child: 
-              Container(
-                child: Column(
-                  children: [
-                    SizedBox(height:10),
-                    Text('Verify your seed phrase',style:TextStyle(fontWeight: FontWeight.bold,fontSize:20,color: Colors.black87)),
-                    SizedBox(height:15),
-                  Text('Choose each word in the correct order',style: TextStyle(fontSize:16,fontWeight: FontWeight.w500),),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Color(0xFF1D1B1D),
-                            borderRadius: BorderRadius.circular(10)),
-                        height: 200,
-                        width: 375,
-                        child: Center(
-                            child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Text('wow',
-                              style: GoogleFonts.firaCode(
-                                  color: Colors.white, fontSize: 25)),
-                        )),
-                      ),
+            child: Container(
+              child: Column(
+                children: [
+                  SizedBox(height: 10),
+                  Text('Verify your seed phrase',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.black87)),
+                  SizedBox(height: 15),
+                  Text(
+                    'Choose each word in the correct order',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xFF1D1B1D),
+                          borderRadius: BorderRadius.circular(10)),
+                      height: 220,
+                      width: 375,
+                      child: tappedChips.length > 0
+                          ? Wrap(
+                              spacing: 0,
+                              runSpacing: 0,
+                              children: tappedChips,
+                            )
+                          : Container(),
                     ),
-                    SizedBox(height: 10,),
-                    
-                    
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Wrap(
+                      spacing: 0.0, // gap between adjacent chips
+                      runSpacing: 0.0, // gap between lines
+                      children: List.generate(
+                          widget.phrases.length,
+                          (index) => chipDesign(widget.phrases[index], index,
+                              Color(0xFFF383638)))),
+                ],
               ),
-            
+            ),
           ),
         ],
       )),
     );
   }
+
+  Widget chipDesign(String label, int index, Color color) => GestureDetector(
+        onTap: () {
+          setState(() {
+            if (!chipProps[index]["isSelected"]) {
+              chipProps[index]["isSelected"] = true;
+              checkStr.add(label);
+              print(checkStr);
+              tappedChips.add(Container(
+                child: Chip(
+                  label: Text(
+                    label,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  backgroundColor: color,
+                  elevation: 4,
+                  shadowColor: Colors.grey[50],
+                  padding: EdgeInsets.all(4),
+                ),
+                margin: EdgeInsets.only(left: 12, right: 12, top: 2, bottom: 2),
+              ));
+            }
+          });
+        },
+        child: Container(
+          child: Chip(
+            label: Text(
+              label,
+              style: TextStyle(
+                color: chipProps[index]["isSelected"]
+                    ? Color(0xFFF383638)
+                    : Colors.white,
+              ),
+            ),
+            backgroundColor: color,
+            elevation: 4,
+            shadowColor: Colors.grey[50],
+            padding: EdgeInsets.all(4),
+          ),
+          margin: EdgeInsets.only(left: 12, right: 12, top: 2, bottom: 2),
+        ),
+      );
 }
